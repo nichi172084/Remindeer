274d2b4e9b934e81e08b6194010e09e4c2a5b1e0
chiniu <kimpoychin131@gmail.com>
2026-02-10 22:16:30 +0800

    feat : add session and scene for loggng in

--- src/app.js
+++ src/app.js
@@ -1,13 +1,18 @@
 import dotenv from 'dotenv'
 import { Telegraf } from "telegraf";
 import { task } from './Commands/TaskComposer/task.composer.js';
+import { stageInstance } from './config/stage.js';
+import { session } from "telegraf";
 
 dotenv.config()
 
 const app = new Telegraf(process.env.BOT_TOKEN)
-app.start((ctx) =>{
-    ctx.reply("I am Remindeer, I can help you remind your task with simple and user friendly approach. But first, do you have an existing account?")
-})
+
+// setup Stage
+app.use(session())
+app.use(stageInstance.middleware())
+app.on('err')
+app.start((ctx) => ctx.scene.enter("LOGIN_ID"))
 
 app.help((ctx) => {
     ctx.reply("Hi")
